1b2e35b1d0ac8356b39b1b893e9939721507524a
Alberto Luebbert M. <albertoluebbert@ideashappy.com>
2024-05-30 11:00:19 -0600

    :sparkles: Finalización métodos delete y update bd

--- ejerciciosClase/alumnas/Grupo22.java
+++ ejerciciosClase/alumnas/Grupo22.java
@@ -7,11 +7,17 @@ public class Grupo22 {
 
     // Parámetros de conexión
     // JDBC - Java DataBase Connectivity
+    // CRUD
+    // Create - Crear un nuevo registro
+    // Read - Leer un registro existente
+    // Update - Actualizar un registro existente
+    // Delete - Eliminar un registro existente
+
     private static String url = "jdbc:mysql://localhost:3306/grupo22";
     private static String usuario = "root";
     private static String password = "buttons";
 
-    public void guardarAlumno(String nombre, int edad) {
+    public void guardarAlumna(String nombre, int edad) {
         String queryInsertar = "INSERT INTO alumnas (nombre, edad) VALUES(?, ?)";
         try (Connection connection = DriverManager.getConnection(url, usuario, password);
                 PreparedStatement preparedStatement = connection.prepareStatement(queryInsertar)) {
@@ -26,7 +32,7 @@ public class Grupo22 {
         }
     }
 
-    public void obtenerAlumnos() {
+    public void obtenerAlumnas() {
         String query = "SELECT * FROM alumnas";
         try (Connection connection = DriverManager.getConnection(url, usuario, password);
                 Statement statement = connection.createStatement();
@@ -45,20 +51,79 @@ public class Grupo22 {
         }
     }
 
+    public void eliminarAlumna(int id) {
+        String queryEliminarAlumnas = "DELETE FROM alumnas WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, usuario, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(queryEliminarAlumnas)) {
+            preparedStatement.setInt(1, id);
+            int registrosEliminados = preparedStatement.executeUpdate();
+            System.out.println("Registros eliminados: " + registrosEliminados);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void actualizarAlumna(int id, String nombre, int edad) {
+        String queryActualizarAlumna = "UPDATE alumnas SET nombre = ?, edad = ? WHERE id = ?";
+
+        try (Connection connection = DriverManager.getConnection(url, usuario, password);
+                PreparedStatement preparedStatement = connection.prepareStatement(queryActualizarAlumna)) {
+            preparedStatement.setString(1, nombre);
+            preparedStatement.setInt(2, edad);
+            preparedStatement.setInt(3, id);
+
+            int registrosActualizados = preparedStatement.executeUpdate();
+            System.out.println("Registros actualizados: " + registrosActualizados);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
         Grupo22 operacionesBD = new Grupo22();
         Scanner scanner = new Scanner(System.in);
 
         try {
 
-            System.out.println("Ingresa el nombre de la alumna");
-            String nombreAlumna = scanner.nextLine();
+            System.out.println(
+                    "Ingresa una opción:\n1.- Insertar Alumno.\n2.- Obtener todos los alumnos.\n3.- Actualizar un alumno existente. \n4.- Eliminar un alumno");
+            int menu = scanner.nextInt();
 
-            System.out.println("Ingresa la edad de la alumna");
-            int edadAlumna = scanner.nextInt();
+            switch (menu) {
+                case 1:
+                    System.out.println("*** Insertar Alumnas ***");
+                    scanner.nextLine();
+                    System.out.println("Ingresa el nombre de la alumna");
+                    String nombreAlumna = scanner.nextLine();
+                    System.out.println("Ingresa la edad de la alumna");
+                    int edadAlumna = scanner.nextInt();
+                    operacionesBD.guardarAlumna(nombreAlumna, edadAlumna);
+                    break;
+                case 2:
+                    System.out.println("*** Obtener todos los alumnos*** ");
+                    operacionesBD.obtenerAlumnas();
+                    break;
+                case 3:
+                    System.out.println("*** Actualizar una alumna existente ***");
 
-            operacionesBD.guardarAlumno(nombreAlumna, edadAlumna);
-            operacionesBD.obtenerAlumnos();
+                    System.out.println("Ingresa el id del alumno a actualizar");
+                    int idAlumnaActualizar = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println("Ingresa el nuevo nombre de la alumna");
+                    String nombreAlumnaActualizar = scanner.nextLine();
+                    System.out.println("Ingresa la nueva edad de la alumna");
+                    int edadAlumnaActualizar = scanner.nextInt();
+                    operacionesBD.actualizarAlumna(idAlumnaActualizar, nombreAlumnaActualizar, edadAlumnaActualizar);
+
+                    break;
+                case 4:
+                    System.out.println("*** Eliminar una alumna ***");
+                    System.out.println("Ingresa el id del alumno a eliminar");
+                    int idAlumnaEliminar = scanner.nextInt();
+                    operacionesBD.eliminarAlumna(idAlumnaEliminar);
+                    break;
+            }
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
